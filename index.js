c30310ceeb21bdd40a81995de2d376f9722b70ba
BashirSidi <bashirsalisusidi@gmail.com>
2021-05-11 05:28:24 +0100

    CRUD App

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const userRoutes = require('./routes/users');
+
+app.use(express.json());
+
+app.use('/users', userRoutes);
+
+app.get('*', (req, res) => {
+	res.json({message: 'Oops... something went wrong!'});
+});
+
+const PORT = 3005;
+app.listen(PORT, (req, res) => {
+	console.log(`CRUD App is Running on port ${PORT}`);
+});
+
